7d6e675167e5a9f7e1984c5d555075249384ff6c
atir09 <atirkhan0906@gmail.com>
2022-10-12 16:13:24 +0530

    uploaded varible code

---
+++ web23-variable-1/problem2.js
@@ -0,0 +1,6 @@
+let name="Atir";
+console.log(name);
+name="Farooque";
+console.log(name);
+name="Saira";
+console.log(name)
---
+++ web23-variable-1/problem3.js
@@ -0,0 +1,6 @@
+let name="Atir";
+let age=19;
+console.log(name);
+console.log(typeof(name));
+console.log(age);
+console.log(typeof (age));
